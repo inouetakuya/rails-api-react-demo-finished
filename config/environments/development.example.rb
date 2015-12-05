5bab71e97c818f79febcf6bdb8aea7e203ce9a76
INOUE Takuya <inouetakuya5@gmail.com>
2015-12-06 04:24:42 +0900

    Add ENV['ORIGIN'] and ENV['OAUTH_CALLBACK']

--- config/environments/development.example.rb
+++ config/environments/development.example.rb
@@ -27,6 +27,8 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   # Set environment secrets
+  ENV['ORIGIN'] = 'http://localhost:8080'
+  ENV['OAUTH_CALLBACK'] = 'http://127.0.0.1:3000/access_token'
   ENV['SECRET_KEY_BASE'] = '55ddbe69b5e1cf8ae25011f404fc1a5a74c12433ad24eb1c09721b9ad8697c3362c20161825c90115a08202704bbe4062ea9cc7387cf755094d702c04b941cbb'
   ENV['TWITTER_CONSUMER_KEY'] = ''
   ENV['TWITTER_CONSUMER_SECRET'] = ''
