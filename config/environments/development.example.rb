fba334d59ac2271ba02ea0de7caf0718e20a72bb
INOUE Takuya <inouetakuya5@gmail.com>
2015-12-06 04:03:46 +0900

    + "webpack-dev-server": "^1.14.0"

--- config/environments/development.example.rb
+++ config/environments/development.example.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
 
   # Set environment secrets
   ENV['SECRET_KEY_BASE'] = '55ddbe69b5e1cf8ae25011f404fc1a5a74c12433ad24eb1c09721b9ad8697c3362c20161825c90115a08202704bbe4062ea9cc7387cf755094d702c04b941cbb'
-  ENV['ORIGIN'] = 'http://127.0.0.1:3000'
+  ENV['ORIGIN'] = 'http://localhost:8080'
   ENV['OAUTH_CALLBACK'] = 'http://127.0.0.1:3000/access_token'
   ENV['TWITTER_CONSUMER_KEY'] = ''
   ENV['TWITTER_CONSUMER_SECRET'] = ''
